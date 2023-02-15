66a980b7a5f656ffaebb903b504721ee1b649cfc
Daniel Loaiza <danieloaiza@live.com>
2023-02-15 13:42:46 -0600

    Updated deck

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,16 @@
 """
-Number of players
-A equals 1 - 11
-5 cards = 21 wins over everything
+Deck class
+Contains the complete deck for the blackjack game
 """
-
 class Deck():
     def __init__(self):
         self.deck = self.create_deck()
         
     def __str__(self):
         return f'The complete deck is: {self.deck}'
+        
+    def updateDeck(self, updatedDeck):
+        self.deck = updatedDeck
         
     def create_deck(self):
         cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
